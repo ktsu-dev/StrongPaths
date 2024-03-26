cfa0f88fed637b199aa118ded3a023062639928c
matt-edmondson <matthew.edmondson@gmail.com>
2024-03-26 19:52:47 +1100

    Add helper properties to top level paths

--- StrongPaths/CommonPaths.cs
+++ StrongPaths/CommonPaths.cs
@@ -4,7 +4,6 @@ namespace ktsu.io.StrongPaths;
 
 using System.Collections.ObjectModel;
 
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2225:Operator overloads have named alternates", Justification = "We're abusing the divide operator here for path syntax so the Divide method doesnt make sense")]
 public sealed record class AbsoluteDirectoryPath : AbsolutePathAbstract<AbsoluteDirectoryPath, IsDirectory>
 {
 	public static AbsoluteFilePath operator /(AbsoluteDirectoryPath left, FileName right) => (AbsoluteFilePath)$"{left}/{right}";
@@ -12,13 +11,34 @@ public sealed record class AbsoluteDirectoryPath : AbsolutePathAbstract<Absolute
 	public static AbsoluteDirectoryPath operator /(AbsoluteDirectoryPath left, RelativeDirectoryPath right) => (AbsoluteDirectoryPath)$"{left}/{right}";
 	public RelativeDirectoryPath RelativeTo(AnyStrongPath other) => AnyRelativePath.Make<RelativeDirectoryPath>(from: this, to: other);
 	public Collection<AnyAbsolutePath> Contents => AnyDirectoryPath.GetContents((AnyDirectoryPath)WeakString);
+	public AbsoluteDirectoryPath Parent => (AbsoluteDirectoryPath)Path.GetDirectoryName(WeakString.Trim(Path.DirectorySeparatorChar).Trim(Path.AltDirectorySeparatorChar));
 }
 public sealed record class AbsoluteFilePath : AbsolutePathAbstract<AbsoluteFilePath, IsFile>
 {
 	public RelativeFilePath RelativeTo(AnyStrongPath other) => AnyRelativePath.Make<RelativeFilePath>(from: this, to: other);
+	/// <summary>
+	///     Returns the last period delimited segment of the FileName including the leading period, or empty if the FileName
+	///     has no extension
+	/// </summary>
+	public FileExtension FileExtension
+	{
+		get
+		{
+			string ext = WeakString.Split(separator: '.').Last();
+			return ext == WeakString ? (FileExtension)string.Empty : (FileExtension)ext.Prepend(element: '.');
+		}
+	}
+
+	/// <summary>
+	///     Returns all trailing period delimited segments of the FileName including the leading period, or empty if the
+	///     FileName has no extensions
+	/// </summary>
+	public FileExtension FullFileExtension => (FileExtension)((string?)WeakString.Split(separator: '.', count: 2).Skip(count: 1).FirstOrDefault()?.Prepend(element: '.') ?? string.Empty);
+
+	public FileName FileName => (FileName)Path.GetFileName(WeakString);
+	public AbsoluteDirectoryPath DirectoryPath => (AbsoluteDirectoryPath)Path.GetDirectoryName(WeakString);
 }
 
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2225:Operator overloads have named alternates", Justification = "We're abusing the divide operator here for path syntax so the Divide method doesnt make sense")]
 public sealed record class RelativeDirectoryPath : RelativePathAbstract<RelativeDirectoryPath, IsDirectory>
 {
 	public static RelativeFilePath operator /(RelativeDirectoryPath left, FileName right) => (RelativeFilePath)$"{left}/{right}";
@@ -26,8 +46,31 @@ public sealed record class RelativeDirectoryPath : RelativePathAbstract<Relative
 	public static RelativeDirectoryPath operator /(RelativeDirectoryPath left, RelativeDirectoryPath right) => (RelativeDirectoryPath)$"{left}/{right}";
 	public RelativeDirectoryPath RelativeTo(AnyStrongPath other) => Make<RelativeDirectoryPath>(from: this, to: other);
 	public Collection<AnyAbsolutePath> Contents => AnyDirectoryPath.GetContents((AnyDirectoryPath)WeakString);
+	public RelativeDirectoryPath Parent => (RelativeDirectoryPath)Path.GetDirectoryName(WeakString.Trim(Path.DirectorySeparatorChar).Trim(Path.AltDirectorySeparatorChar));
 }
 public sealed record class RelativeFilePath : RelativePathAbstract<RelativeFilePath, IsFile>
 {
 	public RelativeFilePath RelativeTo(AnyStrongPath other) => Make<RelativeFilePath>(from: this, to: other);
+
+	/// <summary>
+	///     Returns the last period delimited segment of the FileName including the leading period, or empty if the FileName
+	///     has no extension
+	/// </summary>
+	public FileExtension FileExtension
+	{
+		get
+		{
+			string ext = WeakString.Split(separator: '.').Last();
+			return ext == WeakString ? (FileExtension)string.Empty : (FileExtension)ext.Prepend(element: '.');
+		}
+	}
+
+	/// <summary>
+	///     Returns all trailing period delimited segments of the FileName including the leading period, or empty if the
+	///     FileName has no extensions
+	/// </summary>
+	public FileExtension FullFileExtension => (FileExtension)((string?)WeakString.Split(separator: '.', count: 2).Skip(count: 1).FirstOrDefault()?.Prepend(element: '.') ?? string.Empty);
+
+	public FileName FileName => (FileName)Path.GetFileName(WeakString);
+	public RelativeDirectoryPath DirectoryPath => (RelativeDirectoryPath)Path.GetDirectoryName(WeakString);
 }
---
+++ StrongPaths/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "CA2225:Operator overloads have named alternates", Justification = "<Pending>", Scope = "member", Target = "~M:ktsu.io.StrongPaths.AbsoluteDirectoryPath.op_Division(ktsu.io.StrongPaths.AbsoluteDirectoryPath,ktsu.io.StrongPaths.FileName)~ktsu.io.StrongPaths.AbsoluteFilePath")]
+[assembly: SuppressMessage("Usage", "CA2225:Operator overloads have named alternates", Justification = "<Pending>", Scope = "member", Target = "~M:ktsu.io.StrongPaths.AbsoluteDirectoryPath.op_Division(ktsu.io.StrongPaths.AbsoluteDirectoryPath,ktsu.io.StrongPaths.RelativeDirectoryPath)~ktsu.io.StrongPaths.AbsoluteDirectoryPath")]
+[assembly: SuppressMessage("Usage", "CA2225:Operator overloads have named alternates", Justification = "<Pending>", Scope = "member", Target = "~M:ktsu.io.StrongPaths.AbsoluteDirectoryPath.op_Division(ktsu.io.StrongPaths.AbsoluteDirectoryPath,ktsu.io.StrongPaths.RelativeFilePath)~ktsu.io.StrongPaths.AbsoluteFilePath")]
+[assembly: SuppressMessage("Usage", "CA2225:Operator overloads have named alternates", Justification = "<Pending>", Scope = "member", Target = "~M:ktsu.io.StrongPaths.RelativeDirectoryPath.op_Division(ktsu.io.StrongPaths.RelativeDirectoryPath,ktsu.io.StrongPaths.FileName)~ktsu.io.StrongPaths.RelativeFilePath")]
+[assembly: SuppressMessage("Usage", "CA2225:Operator overloads have named alternates", Justification = "<Pending>", Scope = "member", Target = "~M:ktsu.io.StrongPaths.RelativeDirectoryPath.op_Division(ktsu.io.StrongPaths.RelativeDirectoryPath,ktsu.io.StrongPaths.RelativeDirectoryPath)~ktsu.io.StrongPaths.RelativeDirectoryPath")]
+[assembly: SuppressMessage("Usage", "CA2225:Operator overloads have named alternates", Justification = "<Pending>", Scope = "member", Target = "~M:ktsu.io.StrongPaths.RelativeDirectoryPath.op_Division(ktsu.io.StrongPaths.RelativeDirectoryPath,ktsu.io.StrongPaths.RelativeFilePath)~ktsu.io.StrongPaths.RelativeFilePath")]
